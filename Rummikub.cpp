978b012b56ded752cfef0110e56fdb006021f8fe
Vetrogon <112478930+EvaCheer@users.noreply.github.com>
2026-01-20 13:57:25 +0200

    Clean up comments and srand initialization
    Removed unnecessary comments and srand initialization.

--- Rummikub.cpp
+++ Rummikub.cpp
@@ -1,19 +1,4 @@
 /**
-*
-* Solution to course project # 7
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2025/2026
-*
-* @author Zlatina Georgieva
-* @idnumber 0MI0600109
-* @compiler VC
-*
-* <main file>
-*
-*/
-
-/**
  *
  * Solution to course project # 7
  * Introduction to programming course
@@ -28,12 +13,12 @@
  *
  */
 #include <iostream>
-#include <ctime>
+//#include <ctime>
 #include "Headers.h"
 
 int main()
 {
-	std::srand(std::time(nullptr));
+	//std::srand(std::time(nullptr));
 	Game game;
 	initGame(game);
 	gameRun(game);
